7c3449d1a8f69ffcfc3258e2dac642ae8f34c399
João Vitor Maia Ribeiro <120575645+joaovitorm7@users.noreply.github.com>
2024-12-17 14:05:49 -0300

    alteracao

--- aula06/matriz.java
+++ aula06/matriz.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class matriz {
     public static void main(String[] args) {
         double [][] notas = new double[3][2];
-        double [] medias;
         Scanner ent = new Scanner(System.in);
 
         for(int l = 0; l < notas.length; l++){
@@ -17,7 +16,6 @@ public class matriz {
                 System.out.println("Aluno: "+ (l+1)+ " nota "+ (c+1));
             }
         }
-
         for(int i = 0; i < notas.length; i++){
             double soma = 0;
             for(int j = 0 ; j < notas[i].length; j++){
