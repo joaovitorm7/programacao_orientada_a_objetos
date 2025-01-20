5f9bb327efaa4109a8fba3cdb8aa5c6b63d5d7c6
João Vitor Maia Ribeiro <120575645+joaovitorm7@users.noreply.github.com>
2025-01-20 13:37:16 -0300

    style: alteração no texto do arquivo Main.java

--- aula09/Main.java
+++ aula09/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Livro l1 = new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1954);
         Livro l2 = new Livro("Harry Potter e a Pedra Filosofal", "J.K. Rowling", 1997);
-        Livro l3 = new Livro("Até o Último Homeme", "Mel Gibson", 2016);
+        Livro l3 = new Livro("Até o Último Homem", "Mel Gibson", 2016);
 
         Biblioteca biblioteca = new Biblioteca();
         biblioteca.adicionarLivro(l1);
@@ -20,7 +20,7 @@ public class Main {
         System.out.println("\n--- João pega um livro ---");
         leitor1.pegarLivro(l1);
 
-        System.out.println("Maria pega um livro ---");
+        System.out.println("--- Maria pega um livro ---");
         leitor2.pegarLivro(l2);
 
         System.out.println("\n--- Status dos livros após o empréstimo ---");
@@ -29,8 +29,7 @@ public class Main {
 
         System.out.println("\n--- João devolve o livro ---");
         leitor1.devolverLivro();
-        System.out.println("Maria devolve o livro ---");
-        leitor2.devolverLivro();
+    
 
         System.out.println("\n--- Status dos livros após a devolução ---");
         biblioteca.listarLivrosDisponiveis();
