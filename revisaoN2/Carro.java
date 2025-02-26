4dbbc1b23c05c5c38de02b152058bcab054c3cfe
João Vitor Maia Ribeiro <120575645+joaovitorm7@users.noreply.github.com>
2025-02-26 08:21:12 -0300

    feat: Tornando os atributos privados e criando métodos get e set da classe Carro

--- revisaoN2/Carro.java
+++ revisaoN2/Carro.java
@@ -1,11 +1,31 @@
 public class Carro extends Veiculo{
     // Atributos
-    String modelo;
-    int ano;
+    private String modelo;
+    private int ano;
 
     // Construtor
     public Carro(String modelo, int ano) {
         this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    // Get para obter o modelo do carro
+    public String getModelo(){
+        return modelo;
+    }
+
+    // Setters para definir o modelo do carro
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    // Get para obter o ano do carro
+    public int getAno(){
+        return ano;
+    }
+
+    // Setters para definir o ano do carro
+    public void  setAno(int ano){
         this.ano = ano;
     }
 
