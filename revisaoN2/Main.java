b025212d7ce62eafaddc8e576dae2a43fc4f58bd
João Vitor Maia Ribeiro <120575645+joaovitorm7@users.noreply.github.com>
2025-02-26 08:25:46 -0300

    feat: alterando o arquivo Main.java para testar o encapsulamento de atributos

--- revisaoN2/Main.java
+++ revisaoN2/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Veiculo[] veiculos = { new  Carro("Gol", 2018), new Caminhao("Volvo", 2019), new Veiculo() };
+
+        Carro car = new Carro("Corolla", 2020);
+        car.acelerar();
+
+        car.setModelo("Honda Civic");
+        car.setAno(2020);
+
+        System.out.println("Novo Modelo: " + car.getModelo() + " Ano: " + car.getAno());
+        
+        //Veiculo[] veiculos = { new  Carro("Gol", 2018), new Caminhao("Volvo", 2019), new Veiculo() };
     
-        for (Veiculo veiculo : veiculos) {
-        veiculo.mover();
-        }
+        //for (Veiculo veiculo : veiculos) {
+        //veiculo.mover();
+        //}
     }
 }
